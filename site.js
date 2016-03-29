340a0dac6a00d8179d4ded9b3d3ab98fa86f4bbb
Ulrich Stroetz <uli@allyapp.com>
2016-03-29 19:35:29 -0300

    Add full url of img

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
 $.ajax({
-    url: "images",
+    url: "http://ustroetz.github.io/g/images",
     success: function(data) {
         var images = new Array();
         $(data).find("a:contains(.jpg)").each(function() {
@@ -9,6 +9,7 @@ $.ajax({
 
         });
         var imagePath = 'images/' + images[Math.floor(Math.random() * images.length)];
+        console.log(imagePath);
         $('#start-img').attr('src', imagePath);
     }
 });
